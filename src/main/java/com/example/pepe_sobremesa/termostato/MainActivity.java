f251c2f31af6a5edc0e5941bf3892a8fb6f5c9a0
José Portela <pepeportelamag@gmail.com>
2018-03-01 00:02:13 +0100

    Initial commit

---
+++ src/main/java/com/example/pepe_sobremesa/termostato/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pepe_sobremesa.termostato;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

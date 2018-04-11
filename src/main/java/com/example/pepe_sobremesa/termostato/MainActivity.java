481294929e05c9b20b4e096f81508f687303f86a
José Portela <pepeportelamag@gmail.com>
2018-04-12 01:47:00 +0200

    Added starter socket in local network

--- src/main/java/com/example/pepe_sobremesa/termostato/MainActivity.java
+++ src/main/java/com/example/pepe_sobremesa/termostato/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.pepe_sobremesa.termostato;
 
+import android.content.SyncStatusObserver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Socket socket;
+    private static final int SERVERPORT = 5000;
+    private static final String SERVER_IP = "192.168.0.20";
+    BufferedReader bufferedReader = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Thread(new ClientThread()).start();
+    }
+
+    class ClientThread implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr,SERVERPORT);
+                bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String pepe = bufferedReader.readLine();
+                Log.d("APP",pepe);
+
+            }catch (UnknownHostException e1){
+                e1.printStackTrace();
+            }catch (IOException e1){
+                e1.printStackTrace();
+            }
+        }
     }
 }
